8ac990b542389d16ffedcc0f0e939bbb3ab41544
Manikasethi <manikasethi2005@gmail.com>
2024-12-24 23:15:44 +0530

    first commit

---
+++ qr_code.py
@@ -0,0 +1,4 @@
+import qrcode as qr
+img= qr.make('Hello,I am Manika') #content of QR
+img.save("try.png")  #name of image
+
---
+++ qr_code_2.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   
+                   box_size=10,
+                   border=4,)
+qr.add_data("Namaste!")
+qr.make(fit=True)
+img= qr.make_image(fill_color="pink",back_color="yellow")
+img.save("hindi.png")
